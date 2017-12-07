5d84cec0595858a16ff02bbbeed785e7c530512f
Emile Josso <emile.josso@hotmail.fr>
2017-12-07 22:52:30 +0100

    Page extrêmement super chaude comme un volcan

--- index.php
+++ index.php
@@ -1,8 +1,126 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
+
 <head>
-	<title>Test PHP</title>
+
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="description" content="Un chatbot pour faire des booms">
+	<meta name="author" content="Les guignols de l'info">
+
+	<title>ChatBot Du Sale</title>
+
+	<!-- Bootstrap core CSS -->
+	<link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+	<!-- Custom styles for this template -->
+	<link href="css/shop-homepage.css" rel="stylesheet">
+
 </head>
+
 <body>
-	<?php echo '<p>Bonjour le monde</p>'; ?>
+
+	<!-- Navigation -->
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+		<div class="container">
+			<a class="navbar-brand" href="#">chatbotdusale.com</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarResponsive">
+				<ul class="navbar-nav ml-auto">
+					<li class="nav-item active">
+						<a class="nav-link" href="#">Accueil
+							<span class="sr-only">(current)</span>
+						</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">A propos</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+
+	<!-- Page Content -->
+	<div class="container">
+
+		<div class="row">
+
+			<div class="col-lg-3">
+
+				<h1 class="my-4">Chat Bot Du Sale</h1>
+				<div class="list-group">
+					<a href="#" class="list-group-item">Infos</a>
+					<a href="#" class="list-group-item">Chat</a>
+				</div>
+
+			</div>
+
+			<div class="col-lg-9">
+
+				<div id="carouselExampleIndicators" class="carousel slide my-4" data-ride="carousel">
+						<div class="card h-100">
+							<div class="card-body">
+								<h4 class="card-title">
+									<a href="#">Qui amène quoi</a>
+								</h4>
+								<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
+							</div>
+						</div>
+				</div>
+
+				<div class="row">
+
+					<div class="col-lg-6 col-md-6 mb-4">
+						<div class="card h-100">
+							<a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+							<div class="card-body">
+								<h4 class="card-title">
+									<a href="#">Qui amène quoi</a>
+								</h4>
+								<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-lg-6 col-md-6 mb-4">
+						<div class="card h-100">
+							<a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+							<div class="card-body">
+								<h4 class="card-title">
+									<a href="#">Covoit</a>
+								</h4>
+								<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur! Lorem ipsum dolor sit amet.</p>
+							</div>
+						</div>
+					</div>
+
+
+				</div>
+				<!-- /.row -->
+
+			</div>
+			<!-- /.col-lg-9 -->
+
+		</div>
+		<!-- /.row -->
+
+	</div>
+	<!-- /.container -->
+
+	<!-- Footer -->
+	<footer class="py-5 bg-dark">
+		<div class="container">
+			<p class="m-0 text-center text-white">Copyright &copy; La nuit de l'info et l'internet digital web 2.0 blockchain chat libre gnu gcc sécurité routière</p>
+		</div>
+		<!-- /.container -->
+	</footer>
+
+	<!-- Bootstrap core JavaScript -->
+	<script src="vendor/jquery/jquery.min.js"></script>
+	<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
 </body>
+
 </html>
